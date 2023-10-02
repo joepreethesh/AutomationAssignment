1833340e191b3183e91805382cf27f2a4615770d
joepreethesh <joe.preethesh29@mail.com>
2023-10-02 21:15:18 +0530

    Updated env file

--- features/environment.py
+++ features/environment.py
@@ -5,9 +5,9 @@ from selenium import webdriver
 def before_scenario(context, scenario):
     print("chrome driver executing")
     chrome_driver_path = os.path.abspath(os.path.join('webdriver', 'chromedriver'))
-    print("Chromedriver Path1:", chrome_driver_path)
-    print("System PATH:", os.environ['PATH'])
-    os.environ['PATH'] = f"{os.environ['PATH']}:{os.path.dirname(chrome_driver_path)}"
+    print("Chromedriver Path:", chrome_driver_path)
+    #print("System PATH:", os.environ['PATH'])
+    #os.environ['PATH'] = f"{os.environ['PATH']}:{os.path.dirname(chrome_driver_path)}"
     context.driver = webdriver.Chrome(executable_path=chrome_driver_path)
     context.driver.get("https://www.saucedemo.com")
     time.sleep(5)
